30f65bd873af5655275c6541279af6206ebfa2bc
AIDXYLELELE <warboyka48@gmail.com>
2023-06-01 17:59:02 +0200

    feat: Added integration tests for balancer

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -4,29 +4,103 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"testing"
+	"strconv"
 	"time"
+
+	. "gopkg.in/check.v1"
 )
 
 const baseAddress = "http://balancer:8090"
 
-var client = http.Client{
-	Timeout: 3 * time.Second,
+type Client struct {
+	client http.Client
 }
 
-func TestBalancer(t *testing.T) {
-	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
+func NewClient() *Client {
+	return &Client{
+		client: http.Client{
+			Timeout: 3 * time.Second,
+		},
 	}
+}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+func (c *Client) Get(responseSize int) (*http.Response, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/v1/some-data", baseAddress), nil)
 	if err != nil {
-		t.Error(err)
+		return nil, err
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+	req.Header.Set("Response-Size", strconv.Itoa(responseSize))
+
+	return c.client.Do(req)
 }
 
-func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+type IntegrationSuite struct {
+	client *Client
+}
+
+func (s *IntegrationSuite) SetUpSuite(c *C) {
+	s.client = NewClient()
+}
+
+func (s *IntegrationSuite) TestBalancer(c *C) {
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		c.Skip("Integration test is not enabled")
+	}
+
+	// test server1
+	server1, err := s.client.Get(3072)
+	if err != nil {
+		c.Error(err)
+	}
+
+	server1Header := server1.Header.Get("lb-from")
+	c.Check(server1Header, Equals, "server1:8080")
+
+	// test server2
+	server2, err := s.client.Get(1024)
+	if err != nil {
+		c.Error(err)
+	}
+
+	server2Header := server2.Header.Get("lb-from")
+	c.Check(server2Header, Equals, "server2:8080")
+
+	// test server3
+	server3, err := s.client.Get(2048)
+	if err != nil {
+		c.Error(err)
+	}
+
+	server3Header := server3.Header.Get("lb-from")
+	c.Check(server3Header, Equals, "server3:8080")
+	// again server 2
+	two_server2, err := s.client.Get(1024)
+	if err != nil {
+		c.Error(err)
+	}
+
+	two_server2Header := two_server2.Header.Get("lb-from")
+	c.Check(two_server2Header, Equals, "server2:8080")
+
+	// test server3 again
+	two_server3, err := s.client.Get(1024)
+	if err != nil {
+		c.Error(err)
+	}
+
+	two_server3Header := two_server3.Header.Get("lb-from")
+	c.Check(two_server3Header, Equals, "server3:8080")
+}
+
+func (s *IntegrationSuite) BenchmarkBalancer(c *C) {
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		c.Skip("Integration test is not enabled")
+	}
+
+	for i := 0; i < c.N; i++ {
+		_, err := s.client.Get(10)
+		if err != nil {
+			c.Error(err)
+		}
+	}
 }
